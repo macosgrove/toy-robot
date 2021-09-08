6b062004bd96cfb843af11d53f74cee9f6b1860c
macosgrove <mary-anne.cosgrove@envato.com>
2021-09-08 18:04:19 +1000

    Allow the interpreter to handle commands by passing them through to handlers

--- lib/interpreter.rb
+++ lib/interpreter.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class Interpreter
+  def initialize
+    @handlers = []
+  end
+
+  def add_handler(handler)
+    @handlers << handler
+  end
+
   def process(command)
-    command
+    @handlers.each do |handler|
+      return handler.send(command.to_sym) if handler.respond_to?(command.to_sym)
+    end
+    "Unknown command [#{command}]"
   end
 end
--- spec/interpreter_spec.rb
+++ spec/interpreter_spec.rb
@@ -5,7 +5,23 @@ require_relative "../lib/interpreter"
 describe Interpreter do
   subject(:interpreter) { described_class.new }
 
-  it "echoes the command" do
-    expect(interpreter.process("test")).to eq "test"
+  context "when the interpreter has a handler for the command" do
+    let(:test_handler) { double(test: "Test response") }
+
+    before { interpreter.add_handler(test_handler) }
+
+    it "invokes the command on the handler and returns the response" do
+      expect(interpreter.process("test")).to eq "Test response"
+    end
+  end
+
+  context "when the interpreter does not have a handler for the command" do
+    let(:test_handler) { double(foo: "Bar") }
+
+    before { interpreter.add_handler(test_handler) }
+
+    it "returns an error message" do
+      expect(interpreter.process("test")).to eq "Unknown command [test]"
+    end
   end
 end
